322f9812ee0e309fa6816a5653fd8423b4001ae7
Sebastian Sogamoso <sebasoga@gmail.com>
2012-03-28 01:33:10 -0500

    Set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 StaunchRobots::Application.routes.draw do
   resources :products
 
+  root to: 'products#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
