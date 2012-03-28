d6aa053be4d7fd807d352880ba14421574bb9e23
Sebastian Sogamoso <sebasoga@gmail.com>
2012-03-28 00:23:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StaunchRobots::Application.config.secret_token = 'c0a164c59e704a2b547f6fe6deaba7b2cbfcc0f196c0535a90ab8818c65c43ce03c29ffeccf21ad1b657ca8258488013c96fb5ca12886124abe31bdec3e8df73'
