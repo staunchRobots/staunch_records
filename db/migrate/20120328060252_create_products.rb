1705b8c7e2efe6cdaec27a9db7b393e6da6d5731
Sebastian Sogamoso <sebasoga@gmail.com>
2012-03-28 01:30:44 -0500

    Products resource

---
+++ db/migrate/20120328060252_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.integer :qty
+      t.float :price
+      t.boolean :on_sale
+      t.float :sale_price
+
+      t.timestamps
+    end
+  end
+end
