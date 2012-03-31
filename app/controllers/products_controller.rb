1f37256f0e9cd0cb31968ee15dadf0c5358d2b7e
Sebastian Sogamoso <sebasoga@gmail.com>
2012-03-31 18:56:14 -0500

    Index and show actions for categoteries and products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,17 @@
 class ProductsController < ApplicationController
+  before_filter :
+
   def index
+    @products = @category.products
   end
 
   def show
+    @product = @category.products.find(params[:id])
   end
+
+  private
+
+    def article
+      @category = Category.find(params[:category_id])
+    end
 end
