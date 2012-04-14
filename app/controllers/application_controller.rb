29b92be747f588e36fc362cad02d07895a8690b9
Sebastian Sogamoso <sebasoga@gmail.com>
2012-04-14 15:22:04 -0500

    Change authenticate_admin method to work for not authenticates users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def authenticate_admin
-    redirect_to root_path, notice: 'You should be admin to access that' unless current_user.admin
+    redirect_to root_path, notice: 'You should be admin to access that' if current_user.nil? || !current_user.admin
   end
 end
