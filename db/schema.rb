9394e8e1d1326d55ff8f7eb03d7c8b2d24ee089c
Sebastian Sogamoso <sebasoga@gmail.com>
2012-04-14 15:02:14 -0500

    New DB schema with picture for Product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120411013545) do
+ActiveRecord::Schema.define(:version => 20120413081120) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -28,9 +28,14 @@ ActiveRecord::Schema.define(:version => 20120411013545) do
     t.float    "price"
     t.boolean  "on_sale"
     t.float    "sale_price"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
     t.integer  "category_id"
+    t.string   "slug"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   create_table "users", :force => true do |t|
