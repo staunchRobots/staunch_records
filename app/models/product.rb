86b7af0d91aaf8e676300fe642a6b51de708736e
Sebastian Sogamoso <sebasoga@gmail.com>
2012-04-14 15:01:17 -0500

    Add image to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,20 @@
 class Product < ActiveRecord::Base
-  attr_accessible :album, :artist, :description, :qty, :price, :on_sale, :sale_price, :category_id
+
+  has_attached_file :picture,
+                    styles: { small: "176x176>", big: "500x500>" },
+                    path: ":rails_root/public/system/:attachment/:id/:style/:filename",
+                    url: "/system/:attachment/:id/:style/:filename"
+
+  attr_accessible :album, :artist, :description, :qty, :price, :on_sale, :sale_price, :category_id, :picture
 
   belongs_to :category
 
-  validates_presence_of   :album, :artist, :category_id, :qty, :price
+  validates_presence_of   :album, :artist, :category_id, :qty, :price, :picture
   validates :qty,         numericality: { greater_than_or_equal_to: 0 }
   validates :price,       numericality: { greater_than: 0 }
   validates :sale_price,  numericality: { greater_than: 0, less_than_or_equal_to: :price }
   validates :on_sale ,    inclusion: { in: [true, false] }
+  validates_attachment_size  :picture, in: 0..2.megabytes
 
   searchable do
     text :album, :artist, :description
