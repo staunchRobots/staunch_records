6a5d992529c338f4dc33e98e0d200d3328253b06
Sebastian Sogamoso <sebasoga@gmail.com>
2012-04-14 15:18:35 -0500

    Add namespaced scaffold for products, users and catergories

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,35 +1,89 @@
 class Admin::ProductsController < AdminController
-
+  # GET /admin/products
+  # GET /admin/products.json
   def index
-    @products = Product.all
+    @admin_products = Product.order("album").page(params[:page]).per_page(20)
+
     respond_to do |format|
-      format.html {}
-      format.js { render :text => {:a => 'bbbb'}.to_json }
+      format.html # index.html.erb
+      format.json { render json: @admin_products }
     end
   end
 
-  def new
-    @product = Product.new
-  end
-
-  def edit
-  end
-
-  def create
-    @product = Product.new(params[:product])
+  # GET /admin/products/1
+  # GET /admin/products/1.json
+  def show
+    @admin_product = Product.find(params[:id])
 
     respond_to do |format|
-      if @product.save
-        format.html { redirect_to admin_products_path, notice: "Hurray! Product created" }
+      format.html # show.html.erb
+      format.json { render json: @admin_product }
+    end
+  end
+
+  # GET /admin/products/new
+  # GET /admin/products/new.json
+  def new
+    @admin_product = Product.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @admin_product }
+    end
+  end
+
+  # GET /admin/products/1/edit
+  def edit
+    @admin_product = Product.find(params[:id])
+  end
+
+  # POST /admin/products
+  # POST /admin/products.json
+  def create
+    @admin_product = Product.new(params[:product])
+
+    respond_to do |format|
+      if @admin_product.save
+        format.html { redirect_to [:admin, @admin_product], notice: 'Product was successfully created.' }
+        format.json { render json: @admin_product, status: :created, location: @admin_product }
       else
-        format.html { render :new }
+        format.html { render action: "new" }
+        format.json { render json: @admin_product.errors, status: :unprocessable_entity }
       end
     end
   end
 
+  # PUT /admin/products/1
+  # PUT /admin/products/1.json
   def update
+    @admin_product = Product.find(params[:id])
+    @admin_product.assign_attributes(params[:product])
+
+    @admin_product.valid?
+    Rails.logger.info '!!!!!!!!'
+    Rails.logger.info @admin_product.errors.inspect
+
+    
+    respond_to do |format|
+      if @admin_product.save
+        format.html { redirect_to [:admin, @admin_product], notice: 'Product was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @admin_product.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  def delete
+  # DELETE /admin/products/1
+  # DELETE /admin/products/1.json
+  def destroy
+    @admin_product = Product.find(params[:id])
+    @admin_product.destroy
+
+    respond_to do |format|
+      format.html { redirect_to admin_products_url }
+      format.json { head :no_content }
+    end
   end
 end
