722f2b1a23f24b216cfc97fc6331c2c0de26213d
Sebastian Sogamoso <sebasoga@gmail.com>
2012-04-18 01:12:27 -0500

    Add presence validations to cart and cart_item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,9 @@ class Cart < ActiveRecord::Base
   has_many :cart_items
   has_many :products, through: :cart_items
 
+  validates_presence_of :user_id
+
+  # Creates a cart for a newly created user
   def self.create_user_cart(user_id)
     self.create(user_id: user_id)
   end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,6 @@
 class CartItem < ActiveRecord::Base
   belongs_to :cart
   belongs_to :product
+
+  validates_presence_of :cart_id, :product_id
 end
