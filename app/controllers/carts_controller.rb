5b9459b0e6b6cd8d7170073120468efed97e8447
Sebastian Sogamoso <sebasoga@gmail.com>
2012-04-19 19:53:34 -0500

    Define show method for cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,12 @@
 class CartsController < ApplicationController
+  before_filter :authenticate_user!
+
   def show
+    @cart = current_user.cart
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @cart.products }
+    end
   end
 end
